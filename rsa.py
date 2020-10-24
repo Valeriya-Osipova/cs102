fe8e1a9fd8e3cd8fcc8151b92b99d99d0e796665
valeriya <64967406+Valeriya-Osipova@users.noreply.github.com>
2020-10-24 20:40:03 +0300

    Add files via upload

---
+++ rsa.py
@@ -0,0 +1,88 @@
+import random
+import typing as tp
+def is_prime(n: int) -> bool:
+    """
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    from math import ceil
+    a=n
+    b=0
+    for i in range(2,ceil(a**0.5)+1):
+        if a%i==0:
+            b+=1
+            break
+    if a==1 :
+        return bool(0) 
+    elif a==2:
+        return bool(1)
+    elif b==0:
+        return bool(1)
+    else:
+        return bool(0) 
+    pass
+def gcd(a: int, b: int) -> int:
+    while a!=0 and b!=0:
+        if a>b:
+            a=a%b
+        else:
+            b=b%a
+    return a+b 
+def multiplicative_inverse(e: int, phi: int) -> int:
+    i=1
+    if phi==1:
+        return 0
+    else:
+        while True:
+            if i%e==0:
+                break
+            else:
+                i+=phi
+        return (i//e)
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+    n=p*q
+    phi = (p-1)*(q-1)
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    d = multiplicative_inverse(e, phi)
+    return ((e, n), (d, n))
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return "".join(plain)
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
